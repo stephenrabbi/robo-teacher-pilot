586fd483812bc0e47c2b76d6658c91ec1e03a270
Herbert Stephen <oluwafemiherbert@gmail.com>
2026-09-11 00:44:40 +0100

    Refresh learner navigation after teacher access cleanup

--- classroom/daily_return_fix.js
+++ classroom/daily_return_fix.js
@@ -120,6 +120,6 @@
   if(document.getElementById('roboTeacherNavigationRedesignScript'))return;
   const script=document.createElement('script');
   script.id='roboTeacherNavigationRedesignScript';
-  script.src='/classroom/navigation_redesign.js?v=20260910-nav-p0-1';
+  script.src='/classroom/navigation_redesign.js?v=20260911-nav-p0-2';
   document.body.appendChild(script);
 })();
