245616c3e160bd6d8900ea9811f8db8069648ae4
Herbert Stephen <oluwafemiherbert@gmail.com>
2026-09-11 02:35:56 +0100

    Refresh staging input tools layout

--- classroom/daily_return_fix.js
+++ classroom/daily_return_fix.js
@@ -120,6 +120,6 @@
   if(document.getElementById('roboTeacherNavigationRedesignScript'))return;
   const script=document.createElement('script');
   script.id='roboTeacherNavigationRedesignScript';
-  script.src='/classroom/navigation_redesign.js?v=20260911-nav-p0-3';
+  script.src='/classroom/navigation_redesign.js?v=20260911-nav-p0-4';
   document.body.appendChild(script);
 })();
